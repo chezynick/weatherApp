0b44da958d2278dbe848e81fc1a74a339ee92f83
chezynick <chezynickannie@yahoo.co.uk>
2020-11-13 16:43:18 +0000

    updated JSON folder

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCloud,
   faCloudSun,
-  
   faSun,
+  faWind,
   faSnowflake,
   faCloudRain,
-  
+  faTemperatureHigh,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Weather = ({ currentCity }) => {
@@ -17,13 +17,12 @@ const Weather = ({ currentCity }) => {
   const [currentWind, setCurrentWind] = useState([]);
   const [weatherImage, setWeatherImage] = useState(faSnowflake);
   const [colorBkgrnd, setcolorBkgrnd] = useState("Weather");
-  const [clouds,setClouds] = useState(50);
-  const [cloudier, setCloudier] = useState('cloud2')
-  
+  const [clouds, setClouds] = useState(50);
+  const [cloudier, setCloudier] = useState("cloud2");
+  const [humidity, setHumidity] = useState(0);
 
   //function to get current weather and to update state of weather
   const cityWeather = async (city) => {
-    
     try {
       const response = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=` +
@@ -34,42 +33,42 @@ const Weather = ({ currentCity }) => {
       setCurrentWeather(weather.weather[0].description);
       setCurrentTemp(weather.main.temp.toFixed(0));
       setCurrentWind(weather.wind.speed.toFixed(0));
-      setClouds (weather.clouds.all)
+      setClouds(weather.clouds.all);
+      setHumidity(weather.main.humidity);
     } catch (err) {
       console.log(err, "not worked this");
     }
   };
   //call api get function after changes made to state
-  useEffect(() => cityWeather(currentCity[0].name), [currentCity])
-    useEffect(() => colorist(), [currentTemp])
-    useEffect(() => images(), [clouds])
+  useEffect(() => cityWeather(currentCity[0].name), [currentCity]);
+  useEffect(() => colorist(), [currentTemp]);
+  useEffect(() => images(), [clouds]);
 
   const images = () => {
     //change the weather icon depending on weather
-    
-    if (clouds > 70) {
+
+    if (clouds > 70 && humidity > 85) {
+      setWeatherImage(faCloudRain);
+      setCloudier("cloudVisible");
+    } else if (clouds > 70) {
       setWeatherImage(faCloud);
-      setCloudier('cloudVisible')
-    }
-   else if (clouds > 30 && clouds <70) {
-      setWeatherImage(faCloudSun)
-      setCloudier('cloud2')
-    }
-    else if (clouds <= 30 ) {
+      setCloudier("cloudVisible");
+    } else if (clouds > 30 && clouds < 70) {
+      setWeatherImage(faCloudSun);
+      setCloudier("cloud2");
+    } else if (clouds <= 30) {
       setWeatherImage(faSun);
-      setCloudier('cloud2')
+      setCloudier("cloud2");
     } else {
       setWeatherImage(faCloud);
-      setCloudier('cloud2')
+      setCloudier("cloud2");
     }
   };
 
   const colorist = () => {
-    
     if (Number(currentTemp) < 10) {
       setcolorBkgrnd("WeatherCold");
-    }
-     else if (Number(currentTemp) > 20) {
+    } else if (Number(currentTemp) > 20) {
       setcolorBkgrnd("WeatherHot");
     } else {
       setcolorBkgrnd("Weather");
@@ -84,13 +83,9 @@ const Weather = ({ currentCity }) => {
         size="10x"
         icon={weatherImage}
       />
-      <FontAwesomeIcon
-        className={cloudier}
-        size="7x"
-        icon={faCloud}
-      />
-      <h1 className="temp">{currentTemp}c</h1>
-      <h1 className="wind">{currentWind}mph</h1>
+      <FontAwesomeIcon className={cloudier} size="7x" icon={weatherImage} />
+      <h1 className="temp"><FontAwesomeIcon className='windIcon' size="1x" icon={faTemperatureHigh} />{currentTemp}c</h1>
+      <h1 className="wind">{currentWind}mph<FontAwesomeIcon className='windIcon' size="1x" icon={faWind} /></h1>
     </div>
   );
 };
