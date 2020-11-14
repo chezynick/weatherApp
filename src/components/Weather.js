ac0d586a8bd97d8a863ef0ca462a02fbfd478c86
chezynick <chezynickannie@yahoo.co.uk>
2020-11-14 12:30:59 +0000

    added animations to clouds and sun

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -18,8 +18,9 @@ const Weather = ({ currentCity }) => {
   const [weatherImage, setWeatherImage] = useState(faSnowflake);
   const [colorBkgrnd, setcolorBkgrnd] = useState("Weather");
   const [clouds, setClouds] = useState(50);
-  const [cloudier, setCloudier] = useState("cloud2");
+  const [cloudAnim, setCloudAnim] = useState(false)
   const [humidity, setHumidity] = useState(0);
+  const [spinSun,setSpinSun] = useState (false);
 
   //function to get current weather and to update state of weather
   const cityWeather = async (city) => {
@@ -49,19 +50,24 @@ const Weather = ({ currentCity }) => {
 
     if (clouds > 70 && humidity > 85) {
       setWeatherImage(faCloudRain);
-      setCloudier("cloudVisible");
+      setCloudAnim(true);
+      setSpinSun(false)
     } else if (clouds > 70) {
       setWeatherImage(faCloud);
-      setCloudier("cloudVisible");
+      setCloudAnim(true);
+      setSpinSun(false)
     } else if (clouds > 30 && clouds < 70) {
       setWeatherImage(faCloudSun);
-      setCloudier("cloud2");
+      setCloudAnim(false);
+      setSpinSun(false)
     } else if (clouds <= 30) {
       setWeatherImage(faSun);
-      setCloudier("cloud2");
+      setSpinSun(true)
+      setCloudAnim(false);
     } else {
       setWeatherImage(faCloud);
-      setCloudier("cloud2");
+      setCloudAnim(false);
+      setSpinSun(false)
     }
   };
 
@@ -79,11 +85,15 @@ const Weather = ({ currentCity }) => {
       <h1 className="cityName">{currentCity[0].name}</h1>
       <h1 className="weatherType">{currentWeather}</h1>
       <FontAwesomeIcon
-        className="weatherImage"
+        className={spinSun ? 'weatherImageSpin' :"weatherImage"}
         size="10x"
         icon={weatherImage}
       />
-      <FontAwesomeIcon className={cloudier} size="7x" icon={weatherImage} />
+      <div className={cloudAnim ? '' : 'cloud2'}>
+      <FontAwesomeIcon className='cloudVisible' size="9x" icon={weatherImage} />
+      <FontAwesomeIcon className='cloudVisible2' size="8x" icon={weatherImage} />
+      <FontAwesomeIcon className='cloudVisible3' size="7x" icon={weatherImage} />
+      </div>
       <h1 className="temp"><FontAwesomeIcon className='windIcon' size="1x" icon={faTemperatureHigh} />{currentTemp}c</h1>
       <h1 className="wind">{currentWind}mph<FontAwesomeIcon className='windIcon' size="1x" icon={faWind} /></h1>
     </div>
