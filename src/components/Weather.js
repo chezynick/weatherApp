f12c7341630ef7d482c47c311b4401b5f1660818
chezynick <chezynickannie@yahoo.co.uk>
2020-11-15 15:28:14 +0000

    fixed useeffect warnings

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -18,9 +18,9 @@ const Weather = ({ currentCity }) => {
   const [weatherImage, setWeatherImage] = useState(faSnowflake);
   const [colorBkgrnd, setcolorBkgrnd] = useState("Weather");
   const [clouds, setClouds] = useState(50);
-  const [cloudAnim, setCloudAnim] = useState(false)
+  const [cloudAnim, setCloudAnim] = useState(false);
   const [humidity, setHumidity] = useState(0);
-  const [spinSun,setSpinSun] = useState (false);
+  const [spinSun, setSpinSun] = useState(false);
 
   //function to get current weather and to update state of weather
   const cityWeather = async (city) => {
@@ -42,60 +42,88 @@ const Weather = ({ currentCity }) => {
   };
   //call api get function after changes made to state
   useEffect(() => cityWeather(currentCity[0].name), [currentCity]);
-  useEffect(() => colorist(), [currentTemp]);
-  useEffect(() => images(), [clouds]);
+  useEffect(() => {
+    const colorist = () => {
+      if (Number(currentTemp) < 10) {
+        setcolorBkgrnd("WeatherCold");
+      } else if (Number(currentTemp) > 20) {
+        setcolorBkgrnd("WeatherHot");
+      } else {
+        setcolorBkgrnd("Weather");
+      }
+    };
+    colorist();
+  }, [currentTemp]);
 
-  const images = () => {
-    //change the weather icon depending on weather
+  useEffect(() => {
+    const images = () => {
+      //change the weather icon depending on weather
+  
+      if (clouds > 70 && humidity > 85) {
+        setWeatherImage(faCloudRain);
+        setCloudAnim(true);
+        setSpinSun(false);
+      } else if (clouds > 70) {
+        setWeatherImage(faCloud);
+        setCloudAnim(true);
+        setSpinSun(false);
+      } else if (clouds > 30 && clouds < 70) {
+        setWeatherImage(faCloudSun);
+        setCloudAnim(false);
+        setSpinSun(false);
+      } else if (clouds <= 30) {
+        setWeatherImage(faSun);
+        setSpinSun(true);
+        setCloudAnim(false);
+      } else {
+        setWeatherImage(faCloud);
+        setCloudAnim(false);
+        setSpinSun(false);
+      }
+    };
+    images()}, [clouds]);
 
-    if (clouds > 70 && humidity > 85) {
-      setWeatherImage(faCloudRain);
-      setCloudAnim(true);
-      setSpinSun(false)
-    } else if (clouds > 70) {
-      setWeatherImage(faCloud);
-      setCloudAnim(true);
-      setSpinSun(false)
-    } else if (clouds > 30 && clouds < 70) {
-      setWeatherImage(faCloudSun);
-      setCloudAnim(false);
-      setSpinSun(false)
-    } else if (clouds <= 30) {
-      setWeatherImage(faSun);
-      setSpinSun(true)
-      setCloudAnim(false);
-    } else {
-      setWeatherImage(faCloud);
-      setCloudAnim(false);
-      setSpinSun(false)
-    }
-  };
 
-  const colorist = () => {
-    if (Number(currentTemp) < 10) {
-      setcolorBkgrnd("WeatherCold");
-    } else if (Number(currentTemp) > 20) {
-      setcolorBkgrnd("WeatherHot");
-    } else {
-      setcolorBkgrnd("Weather");
-    }
-  };
+  
+  
   return (
     <div className={colorBkgrnd}>
       <h1 className="cityName">{currentCity[0].name}</h1>
       <h1 className="weatherType">{currentWeather}</h1>
       <FontAwesomeIcon
-        className={spinSun ? 'weatherImageSpin' :"weatherImage"}
+        className={spinSun ? "weatherImageSpin" : "weatherImage"}
         size="10x"
         icon={weatherImage}
       />
-      <div className={cloudAnim ? '' : 'cloud2'}>
-      <FontAwesomeIcon className='cloudVisible' size="9x" icon={weatherImage} />
-      <FontAwesomeIcon className='cloudVisible2' size="8x" icon={weatherImage} />
-      <FontAwesomeIcon className='cloudVisible3' size="7x" icon={weatherImage} />
+      <div className={cloudAnim ? "" : "cloud2"}>
+        <FontAwesomeIcon
+          className="cloudVisible"
+          size="9x"
+          icon={weatherImage}
+        />
+        <FontAwesomeIcon
+          className="cloudVisible2"
+          size="8x"
+          icon={weatherImage}
+        />
+        <FontAwesomeIcon
+          className="cloudVisible3"
+          size="7x"
+          icon={weatherImage}
+        />
       </div>
-      <h1 className="temp"><FontAwesomeIcon className='windIcon' size="1x" icon={faTemperatureHigh} />{currentTemp}c</h1>
-      <h1 className="wind">{currentWind}mph<FontAwesomeIcon className='windIcon' size="1x" icon={faWind} /></h1>
+      <h1 className="temp">
+        <FontAwesomeIcon
+          className="windIcon"
+          size="1x"
+          icon={faTemperatureHigh}
+        />
+        {currentTemp}c
+      </h1>
+      <h1 className="wind">
+        {currentWind}mph
+        <FontAwesomeIcon className="windIcon" size="1x" icon={faWind} />
+      </h1>
     </div>
   );
 };
